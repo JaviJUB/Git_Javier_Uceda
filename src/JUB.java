17d79dca6cb6be7dd07de8418d1fe136c2d8e827
Juceda <javier.ucebar@gmail.com>
2023-01-21 17:56:32 +0100

    Modificación 3

--- src/JUB.java
+++ src/JUB.java
@@ -2,6 +2,6 @@
 public class JUB {
 	public static void main(String[] args) {
 		System.out.println("Alumno: Javier Uceda");
-		System.out.println("Modificación desde Master");
+		System.out.println("Modificación desde Master - Cambiado");
 	}
 }
